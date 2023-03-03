5319a196855e95af59fe4aff890e122111d277be
hyq5436 <hyq5436@hotmail.com>
2023-03-03 20:34:09 +0800

    Add project files.

---
+++ GenerateVersionWithCMake/GenerateVersionWithCMake.cpp
@@ -0,0 +1,12 @@
+﻿// GenerateVersionWithCMake.cpp : Defines the entry point for the application.
+//
+
+#include "GenerateVersionWithCMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
